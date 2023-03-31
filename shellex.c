4c3e5829c09bd55b44cbb1272a8f4c2944fee1bf
xitxxth <jongyn1128@gmail.com>
2023-03-31 15:21:45 +0900

    *

--- shellex.c
+++ shellex.c
@@ -89,7 +89,7 @@ int builtin_command(char **argv)
         fseek(fp, 0, SEEK_END);//move to EOF
         return 1;//pass execve
     }
-    if(strncmp(argv[0], "!", 1)){//"!"
+    if(strncmp(*(argv[0]), "!", 1)){//"!"
         if(!strcmp(argv[0], "!!")) {//"!!"
             char tmpCmd[MAXLINE];//for the last cmd
             fseek(fp, 0, SEEK_SET);//reset file cursor
@@ -103,6 +103,7 @@ int builtin_command(char **argv)
             *(argv[0]) = 0;
             int num = atoi(argv[0]);
             printf("%d\n", num);
+            return 1;
         }
     }
     /*else if(!strcmp(*(argv[0]), "!")){
