82fe9db6f91eacb97c5e98bbe83caefa06c981ae
xitxxth <jongyn1128@gmail.com>
2023-04-03 21:58:19 +0900

    latest

--- shellex.c
+++ shellex.c
@@ -13,6 +13,8 @@ int parseline(char *buf, char **argv);
 int builtin_command(char **argv); 
 //User defined
 FILE* fp;
+void pipe_handler(char **argv, int idx);
+int pipe_counter(char **argv, int *arr);
 
 int main() 
 {
@@ -33,10 +35,12 @@ int main()
         char lastCmd[MAXLINE];
         fseek(fp, 0, SEEK_SET);//reset file cursor
         while((fgets(lastCmd, MAXLINE, fp))!=NULL) {}//to EOF
-        fseek(fp, 0, SEEK_END);//move to EOF
         if(strcmp(lastCmd, cmdline)!=0)//not repeated history
             fprintf(fp, "%s", cmdline);
         }//save cmd lines in history.txt
+
+
+
 	if (feof(stdin)){
         if(fclose(fp))
             printf("FILE CLOSE ERROR\n");
@@ -57,22 +61,32 @@ void eval(char *cmdline)
     char buf[MAXLINE];   /* Holds modified command line */
     int bg;              /* Should the job run in bg or fg? */
     pid_t pid;           /* Process id */
-    
+    //
+    int status;//var for wait
+    int pipe=0;//pipe flag, 0==off | 1==on
+    int arr[MAXARGS];
+    for(int i=0; i<MAXARGS; i++)    arr[i]=-1;
+
     strcpy(buf, cmdline);
     bg = parseline(buf, argv); 
     if (argv[0] == NULL)  
 	return;   /* Ignore empty lines */
+
+
+    int idx = pipe_counter(argv);
+    for(int i=0; arr[i]>-1; i++){
+        printf("Saved in %d\n",arr[i]);
+    }
+    printf("cnt: %d\n", idx);
+    user defined execve
     if (!builtin_command(argv)) { //quit -> exit(0), & -> ignore, other -> run
             if((pid = Fork())==0){//child
-            execve(argv[0], argv, environ);
-            exit(0);
+            execve(argv[0], argv, environ);//execute and dead
         }
         else{
-            Wait();
+            Wait(&status);
         }
-        /*if (execve(argv[0], argv, environ) < 0) {	//ex) /bin/ls ls -al &
-            printf("%s: Command not found.\n", argv[0]);
-            exit(0);*/
+    
         }
 
 
@@ -82,7 +96,6 @@ void eval(char *cmdline)
 	}
 	else//when there is backgrount process!
 	    printf("%d %s", pid, cmdline);
-    }
     return;
 }
 
@@ -111,7 +124,6 @@ int builtin_command(char **argv)
             char tmpCmd[MAXLINE];//for the last cmd
             fseek(fp, 0, SEEK_SET);//reset file cursor
             while((fgets(tmpCmd, MAXLINE, fp))!=NULL) {}//to EOF
-            fseek(fp, 0, SEEK_END);//move to EOF
             printf("last command is %s", tmpCmd);//print last cmd
             eval(tmpCmd);//execute it
             return 1;       
@@ -133,7 +145,6 @@ int builtin_command(char **argv)
             char lastCmd[MAXLINE];
             fseek(fp, 0, SEEK_SET);//reset file cursor
             while((fgets(lastCmd, MAXLINE, fp))!=NULL) {}//to EOF
-            fseek(fp, 0, SEEK_END);//move to EOF
             if(strcmp(lastCmd, tmpCmd)!=0)//not repeated history
                 fprintf(fp, "%s", tmpCmd);//save cmd lines in history.txt
             if(foundFlag)   eval(tmpCmd);//run found execution
@@ -143,7 +154,8 @@ int builtin_command(char **argv)
 
     if(strncmp("cd", argv[0], 2)==0){//"cd"
         if(argv[1]==NULL || *argv[1]=='~'){//cd, cd ~
-            chdir(getenv("HOME"));//cd home
+            int set = chdir(getenv("HOME"));//cd home
+            //var set has no role, mask warning message
         }
         else{
             if(chdir(argv[1])==-1){//chdir failed
@@ -189,3 +201,46 @@ int parseline(char *buf, char **argv)
     return bg;
 }
 /* $end parseline */
+
+//proto-funciton for 1 | 1 | 1 ...
+/*
+void pipe_handler(char** argv, int idx)
+{// handle mine >> | exists? >> pass it >> done , idx starts from 1
+    int fd[2];
+    pipe(fd);//commuicate with child of mine, fd[0] == read, fd[1] == write
+    pid_t pid;           // Process id 
+    int status;
+    int pipe_flag=0; //pipe flag, child exists!
+    // if(*(argv[idx+1])=='|'){
+    //     pipe_flag=1;
+    //     idx+=2;
+    // }
+    if (!builtin_command(argv)) { //quit -> exit(0), & -> ignore, other -> run
+            if((pid = Fork())==0){//child
+            if(pipe_flag){
+                dup2(fd[1], stdout);
+                pipe_handler(argv, idx);
+            }
+            execve(argv[0], argv, environ);//execute and dead
+        }
+        else{
+            if(pipe_flag){
+                dup2(fd[0], stdin);
+            }
+            Waitpid(pid, &status, 0);
+        }
+    }
+}
+*/
+
+int pipe_counter(char** argv)
+{   
+    int cnt=0, k=0;
+    for(int i=0; argv[i]; i++){
+        if(*(argv[i])=="|"){
+            cnt++;
+            arr[k++]=i;//arr[k] = | saved index
+        }
+    }
+    return cnt;
+}
