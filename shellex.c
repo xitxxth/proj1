f1c947c0b6155a0c42a02353cae83b31519d790a
xitxxth <jongyn1128@gmail.com>
2023-04-06 22:22:45 +0900

    ?

--- shellex.c
+++ shellex.c
@@ -301,8 +301,8 @@ pid_t pipe_handler(char** argv, int* arr, int idx)
                 pipe_handler(argv, arr, idx+1);
             }
             else{   
-                printf("%d: %s is exectued!!\n", pid, parsedArgv[0]);
-                if(execvp(parsedArgv[0], parsedArgv)<0) {
+                //printf("%d: %s is exectued!!\n", pid, parsedArgv[0]);
+                //if(execvp(parsedArgv[0], parsedArgv)<0) {
                 printf("%s:Command not found.\n", argv[0]);
                 return 0;
                 }
